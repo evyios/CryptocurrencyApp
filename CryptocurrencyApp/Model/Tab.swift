8604193680af6d33feccd4a094f4804a06c1f12d
Evgeny Yachny <ev.yachny@gmail.com>
2025-02-22 11:38:25 +0300

    enum for TabView added

---
+++ CryptocurrencyApp/Model/Tab.swift
@@ -0,0 +1,15 @@
+//
+//  Tab.swift
+//  CryptocurrencyApp
+//
+//  Created by Evgeny on 22.02.25.
+//
+
+import SwiftUI
+
+enum Tab: String, CaseIterable {
+    case home = "Home"
+    case note = "Note"
+    case settings = "Settings"
+    case profile = "Profile"
+}
