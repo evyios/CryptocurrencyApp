50f3eb040755edbe4cd624989a77e2fda5a1925d
Evgeny Yachny <ev.yachny@gmail.com>
2025-02-23 10:59:06 +0300

    Custom TabBar + CustomCorner

---
+++ CryptocurrencyApp/Views/CustomCorner.swift
@@ -0,0 +1,18 @@
+//
+//  CustomCorner.swift
+//  CryptocurrencyApp
+//
+//  Created by Evgeny on 22.02.25.
+//
+
+import SwiftUI
+
+struct CustomCorner: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CustomCorner()
+}
