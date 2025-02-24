cf64b84d62a8fa5421f46b9f1570eaa734fd18b3
Evgeny Yachny <ev.yachny@gmail.com>
2025-02-24 21:20:01 +0300

    home header added

--- CryptocurrencyApp/Views/Home.swift
+++ CryptocurrencyApp/Views/Home.swift
@@ -9,7 +9,55 @@ import SwiftUI
 
 struct Home: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ScrollView(.vertical) {
+            VStack(spacing: 15) {
+                HomeHeader()
+            }
+            .padding(15)
+            // Поскольку Tab Bar находится в ZStack
+            .padding(.bottom, 50)
+        }
+        .padding(.bottom, 5)
+    }
+    
+    @ViewBuilder
+    func HomeHeader() -> some View {
+        HStack(spacing: 13) {
+            Text("Cryptocurrency")
+                .font(.title)
+                .fontWeight(.bold)
+                .frame(maxWidth: .infinity, alignment: .leading)
+                .padding(.leading, 10)
+            
+            Button {
+                
+            } label: {
+                Image(systemName: "bell.badge.fill")
+                    .renderingMode(.original)
+                    .foregroundStyle(.gray)
+                    .font(.title3)
+                    .frame(width: 40, height: 40)
+                    .background {
+                        Circle()
+                            .stroke(Color.gray.opacity(0.4), lineWidth: 2)
+                    }
+            }
+            Button {
+            
+            } label: {
+                Image(systemName: "magnifyingglass")
+                    .font(.title3)
+                    .foregroundStyle(.gray)
+                    .frame(width: 40, height: 40)
+
+                    .rotationEffect(Angle(degrees: 90))
+                    .background {
+                        Circle()
+                            .stroke(Color.gray.opacity(0.4), lineWidth: 2)
+                    }
+            }
+
+        }
     }
 }
 
