b5e144b0de7ef5778c0834d83fa7e91443414791
Evgeny Yachny <ev.yachny@gmail.com>
2025-03-29 12:05:21 +0300

    custom text field

--- CryptocurrencyApp/Views/NewExpense.swift
+++ CryptocurrencyApp/Views/NewExpense.swift
@@ -8,11 +8,64 @@
 import SwiftUI
 
 struct NewExpense: View {
+    
+    @State private var title: String = ""
+    @State private var subtitle: String = ""
+    @State private var amount: Double = 0
+    @State private var date: Date = .now
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ScrollView(.vertical) {
+            VStack(spacing: 15) {
+                CustomTextField("Title", "Coin Name", value: $title)
+                CustomTextField("Description", "Type something here...", value: $subtitle)
+                
+            
+            }
+            .padding(15)
+        }
+        .navigationTitle("Add new expense")
+    }
+    
+    @ViewBuilder
+    func CustomTextField(_ title: String, _ hint: String, value: Binding<String>) -> some View {
+        VStack(alignment: .leading, spacing: 10) {
+            Text(title)
+                .font(.caption)
+                .foregroundStyle(.gray)
+                .frame(maxWidth: .infinity, alignment: .leading)
+            
+            if title == "Description" {
+                TextField(hint, text: value)
+                    .padding(.horizontal, 15)
+                    .padding(.vertical,12)
+                    .frame(height: 80, alignment: .topLeading)
+                    .background {
+                        RoundedRectangle(cornerRadius: 10)
+                            .fill(Color.gray.opacity(0.15))
+                    }
+                    
+            } else {
+                TextField(hint, text: value)
+                    .padding(.horizontal, 15)
+                    .padding(.vertical,12)
+                    .background {
+                        RoundedRectangle(cornerRadius: 10)
+                            .fill(Color.gray.opacity(0.15))
+                    }
+            }
+        }
+    }
+    
+    var numberFormatter: NumberFormatter {
+        let formatter = NumberFormatter()
+        formatter.numberStyle = .decimal
+        formatter.maximumFractionDigits = 2
+        return formatter
     }
 }
 
 #Preview {
-    NewExpense()
+    NavigationStack {
+        NewExpense()
+    }
 }
