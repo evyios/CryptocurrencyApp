45a0073d173333cae0b482cc88896e9da3aa6ef4
Evgeny Yachny <ev.yachny@gmail.com>
2025-03-29 12:04:19 +0300

    new expense view with custom text field

---
+++ CryptocurrencyApp/Views/NewExpense.swift
@@ -0,0 +1,18 @@
+//
+//  NewExpense.swift
+//  CryptocurrencyApp
+//
+//  Created by Evgeny on 26.03.25.
+//
+
+import SwiftUI
+
+struct NewExpense: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NewExpense()
+}
