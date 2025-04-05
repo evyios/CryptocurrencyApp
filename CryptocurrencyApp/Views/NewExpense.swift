36bd33f0ec2a45619e889274c54ae140dc71e35f
Evgeny Yachny <ev.yachny@gmail.com>
2025-04-05 15:30:20 +0300

    new expense ui change

--- CryptocurrencyApp/Views/NewExpense.swift
+++ CryptocurrencyApp/Views/NewExpense.swift
@@ -14,57 +14,78 @@ struct NewExpense: View {
     @State private var amount: Double = 0
     @State private var date: Date = .now
     var body: some View {
-        ScrollView(.vertical) {
-            VStack(spacing: 15) {
-                CustomTextField("Title", "Coin Name", value: $title)
-                CustomTextField("Description", "Type something here...", value: $subtitle)
-                
-                VStack(alignment: .leading, spacing: 10) {
-                    Text("Price $")
-                        .font(.caption)
-                        .foregroundStyle(.gray)
-                        .frame(maxWidth: .infinity, alignment: .leading)
+        VStack {
+            HStack(spacing: 20) {
+                Button {
                     
-                    TextField("0.00", value: $amount, formatter: numberFormatter)
-                        .padding(.horizontal, 15)
-                        .padding(.vertical,12)
-                        .foregroundStyle(.gray)
-                        .background {
-                            RoundedRectangle(cornerRadius: 10)
-                                .fill(Color.gray.opacity(0.15))
-                        }
-                        .frame(maxWidth: 150)
-                        .keyboardType(.decimalPad)
+                } label: {
+                    Image(systemName: "arrow.left.circle")
+                        .font(.title3)
+                        .tint(.black)
                 }
                 
-                VStack(alignment: .leading, spacing: 10) {
-                    Text("Date")
-                        .font(.caption)
-                        .foregroundStyle(.gray)
-                        .frame(maxWidth: .infinity, alignment: .leading)
+                Text("Add new expense")
+                    .font(.headline)
+                Spacer()
+                Button("Save", action: save)
+
+            }
+            .frame(maxWidth: .infinity, alignment: .leading)
+            .padding(.horizontal,15)
+            .padding(.top,15)
+            
+            ScrollView(.vertical) {
+                VStack(spacing: 15) {
+                    CustomTextField("Title", "Coin Name", value: $title)
+                    CustomTextField("Description", "Type something here...", value: $subtitle)
                     
-                    DatePicker("", selection: $date, displayedComponents: [.date])
-                        .datePickerStyle(.graphical)
-                        .padding(.horizontal, 15)
-                        .padding(.vertical, 5)
-
-                        .background {
-                            RoundedRectangle(cornerRadius: 20)
-                                .fill(Color.gray.opacity(0.15))
-                        }
-
+                    VStack(alignment: .leading, spacing: 10) {
+                        Text("Price $")
+                            .font(.caption)
+                            .foregroundStyle(.gray)
+                            .frame(maxWidth: .infinity, alignment: .leading)
+                        
+                        TextField("0.00", value: $amount, formatter: numberFormatter)
+                            .padding(.horizontal, 15)
+                            .padding(.vertical,12)
+                            .foregroundStyle(.gray)
+                            .background {
+                                RoundedRectangle(cornerRadius: 10)
+                                    .fill(Color.gray.opacity(0.15))
+                            }
+                            .frame(maxWidth: 150)
+                            .keyboardType(.decimalPad)
+                    }
+                    
+                    VStack(alignment: .leading, spacing: 10) {
+                        Text("Date")
+                            .font(.caption)
+                            .foregroundStyle(.gray)
+                            .frame(maxWidth: .infinity, alignment: .leading)
+                        
+                        DatePicker("", selection: $date, displayedComponents: [.date])
+                            .datePickerStyle(.graphical)
+                            .padding(.horizontal, 15)
+                            .padding(.vertical, 5)
+                        
+                            .background {
+                                RoundedRectangle(cornerRadius: 20)
+                                    .fill(Color.gray.opacity(0.15))
+                            }
+                        
+                    }
                 }
-            }
-            .padding(15)
-        }
-        .navigationTitle("Add new expense")
-        .toolbar {
-            ToolbarItem(placement: .topBarTrailing) {
-                Button("Save") {
-                    save()
-                }
+                .padding(15)
             }
         }
+//        .navigationTitle("Add new expense")
+//        .toolbar {
+//            ToolbarItem(placement: .topBarTrailing) {
+//                Button("Save") {
+//                    save()
+//                }
+//            }
+//        }
     }
     
     func save() {
