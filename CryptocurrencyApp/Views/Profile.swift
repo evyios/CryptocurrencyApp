d04d7a2eca9124ba7de2a5f52330424d16800705
Evgeny Yachny <ev.yachny@gmail.com>
2025-04-19 20:27:08 +0300

    profile person image

--- CryptocurrencyApp/Views/Profile.swift
+++ CryptocurrencyApp/Views/Profile.swift
@@ -16,6 +16,14 @@ struct Profile: View {
                         .font(.title)
                         .fontWeight(.bold)
                         .frame(maxWidth: .infinity, alignment: .leading)
+                    
+                    VStack(spacing: 15) {
+                        Image(systemName: "person.crop.circle.fill")
+                            .resizable()
+                            .aspectRatio(contentMode: .fill)
+                            .frame(width: 70, height: 70)
+                            
+                    }
                 }
                 .padding(.horizontal,20)
                 .padding(.vertical,20)
