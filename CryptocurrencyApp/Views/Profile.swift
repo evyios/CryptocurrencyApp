629d3507b2124590e0a6f227f3285339d5b7c84e
Evgeny Yachny <ev.yachny@gmail.com>
2025-04-21 12:39:50 +0300

    profile section complete

--- CryptocurrencyApp/Views/Profile.swift
+++ CryptocurrencyApp/Views/Profile.swift
@@ -17,13 +17,45 @@ struct Profile: View {
                         .fontWeight(.bold)
                         .frame(maxWidth: .infinity, alignment: .leading)
                     
-                    VStack(spacing: 15) {
+                    VStack(spacing: 12) {
                         Image(systemName: "person.crop.circle.fill")
                             .resizable()
                             .aspectRatio(contentMode: .fill)
                             .frame(width: 70, height: 70)
+                            .offset(y: -30)
+                            .padding(.bottom,-30)
+                        
+                        Text("Evgeny Yachny")
+                            .font(.title2)
+                            .fontWeight(.semibold)
+                        
+                        HStack(spacing: 11) {
+                            Image(systemName: "house")
+                                .foregroundStyle(.main)
+                                .offset(x: -2)
                             
+                            Text("Address: Belarus, Minsk")
+                                .font(.caption)
+                        }
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                        
+                        HStack(spacing: 15) {
+                            Image(systemName: "phone.circle")
+                                .foregroundStyle(.main)
+                        
+                            Text("Phone: + 375 (29) 187-41-52")
+                                .font(.caption)
+                        }
+                        .frame(maxWidth: .infinity, alignment: .leading)
                     }
+                    .padding([.horizontal,.bottom])
+                    .background {
+                        RoundedRectangle(cornerRadius: 15)
+                            .fill(Color.gray.opacity(0.15))
+                    }
+                    .padding()
+                    .padding(.top,10)
+                    
                 }
                 .padding(.horizontal,20)
                 .padding(.vertical,20)
