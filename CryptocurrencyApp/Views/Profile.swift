5395fd1b85ab2e0f2eb135740e9a77c2cd07c676
Evgeny Yachny <ev.yachny@gmail.com>
2025-06-10 20:14:56 +0300

    matchedGeometryEffect for image theme

--- CryptocurrencyApp/Views/Profile.swift
+++ CryptocurrencyApp/Views/Profile.swift
@@ -96,6 +96,7 @@ struct Profile: View {
                             .aspectRatio(contentMode: .fill)
                             .frame(width: 50, height: 50)
                             .foregroundStyle(.gray.opacity(0.4), .yellow)
+                            .matchedGeometryEffect(id: "IMAGE", in: animation)
                         
                         Text("Customize your interface. Choose between dark mode and light mode")
                             .font(.subheadline)
