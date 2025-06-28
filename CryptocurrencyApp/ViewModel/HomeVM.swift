22131884efd6c97e89d8725d16d50da4b428bf2d
Evgeny Yachny <ev.yachny@gmail.com>
2025-06-28 17:53:14 +0300

    combine search

--- CryptocurrencyApp/ViewModel/HomeVM.swift
+++ CryptocurrencyApp/ViewModel/HomeVM.swift
@@ -13,6 +13,8 @@ class HomeVM: ObservableObject {
     
     @Published var allCoins: [Coin] = []
     
+    @Published var searchText: String = ""
+    
     private let fetchCoins = DataSource()
     private var cancellables: Set<AnyCancellable> = []
     
@@ -26,5 +28,20 @@ class HomeVM: ObservableObject {
                 self?.allCoins = downloadedCoins
             }
             .store(in: &cancellables)
+        
+        $searchText
+            .combineLatest(fetchCoins.$allCoins)
+            .map { (text, strCoins) -> [Coin] in
+                guard !text.isEmpty else {
+                    return strCoins
+                }
+                let lowercaseText = text.lowercased()
+                let filteredCoins: [Coin] = strCoins.filter { (coin) -> Bool in
+                    return coin.name.lowercased().contains(lowercaseText) ||
+                    coin.symbol.lowercased().contains(lowercaseText) ||
+                    coin.id.lowercased().contains(lowercaseText)
+                }
+                return filteredCoins
+            }
     }
 }
